06c76e46c281b6a8ad99cda26613968612412fff
LamNguy <lamchipabc@gmail.com>
2021-06-04 17:45:21 +0000

    v8.0

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup, find_packages
+with open ("README.md", "r") as f:
+	long_description = f.read()
 
 setup(
-    name='cli-pat',
-    version='0.1.0',
+    name='CLI Proxy Service',
+    version='0.1.1',
+    author='Nguyen Duc Lam',
+    author_email='lamchipabc@gmail.com',
+    description='CLI for Proxy Service integrated with OpenStack',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = 'https://github.com/LamNguy/cli_pat',
+    python_requires='>=2.7',
     packages=find_packages(include=['cli-pat', 'modules.*']),
     install_requires= [
 	'pick',
-	'configparser==4.0.2',
-	'prettytable==1.0.1'], 
+	'pbr>=2.0.0',
+	'configparser',
+	'prettytable',
+	'openstacksdk==0.36.5'], 
     #scripts=['run/proxy-install'],
     entry_points = {
         'console_scripts': ['cli-proxy=modules.cli:main'],
