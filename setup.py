99305a49f2543a5a12b503cec5977218a3c5d9d7
LamNguy <lamchipabc@gmail.com>
2021-06-04 20:47:47 +0000

    v8.2

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 from setuptools import setup, find_packages
 with open ("README.md", "r") as f:
-	long_description = f.read()
+	readme = f.read()
 
 setup(
     name='cli-proxy-openstack',
-    version='0.1.1',
+    version='0.1.4',
     author='Nguyen Duc Lam',
     author_email='lamchipabc@gmail.com',
     description='CLI for Proxy Service integrated with OpenStack',
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
+    long_description_content_type="text/markdown",
     url = 'https://github.com/LamNguy/cli_pat',
     python_requires='>=2.7',
-    packages=find_packages(include=['cli-pat', 'modules.*']),
+    packages=['modules'],
+    include_package_data=True,
     install_requires= [
 	'pick',
 	'pbr>=2.0.0',
 	'configparser',
 	'prettytable',
 	'openstacksdk==0.36.5'], 
-    #scripts=['run/proxy-install'],
     entry_points = {
-        'console_scripts': ['cli-proxy=modules.cli:main'],
+        'console_scripts': ['cli-proxy=modules.__main__:main'],
     }
 )
